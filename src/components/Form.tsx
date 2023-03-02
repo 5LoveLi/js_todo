12f6d42db48644e59a0a60a0e9db07d1335a19d9
Lyba Lila <lila.lyba5@yandex.ru>
2023-03-02 19:51:17 +0500

    eding form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { TaskData } from '../type/Task.types';
+import { TaskList } from './TaskList';
 
+interface IFormTaskProps {
+  onCreate: (task: TaskData) => void;
+}
 
-export const FormTask = () => {
+export const FormTask = ({ onCreate }: IFormTaskProps) => {
+  const [inputTask, setInputTask] = useState('');
+
+  const createTask = (event: any) => {
+    event.preventDefault();
+    const task = {
+      id: new Date().toDateString(),
+      name: inputTask,
+      status: false,
+    };
+    onCreate(task);
+  }
+
+  const updateInputValue = (evt: any) => {
+    console.log(evt.target.value)
+    setInputTask(evt.target.value);
+  }
+
   return (
     <div>
-      <input type="text" />
-      <button>Создать</button>
+      <form>
+        <input type="text"
+          value={inputTask}
+          onChange={updateInputValue} />
+        <input type='submit' value='Создать' onClick={createTask} />
+      </form>
     </div>
   )
 }
