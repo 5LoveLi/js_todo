e1cf29eef88d42261648d5445a933b3946f666f7
Lyba Lila <lila.lyba5@yandex.ru>
2023-03-06 17:36:38 +0500

    create bad filter

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,29 @@
 
 
-export const Footer = () => {
+interface IFooterProps {
+  onClick: (status: boolean) => void,
+}
+
+export const Footer = ({ onClick }: IFooterProps) => {
+
+  // const all = () => {
+  //   onClick(true)
+
+  // }
+
+  const completed = () => {
+    onClick(true)
+  }
+
+  const active = () => {
+    onClick(false)
+  }
 
   return (
     <div>
-      <button>все</button>
-      <button>выполненные</button>
-      <button>активные</button>
+      {/* <button type="submit" onClick={all}>все</button> */}
+      <button type="submit" onClick={completed}>выполненные</button>
+      <button type="submit" onClick={active}>активные</button>
     </div>
   )
 } 
--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
 import { useState } from 'react';
+import { TaskData } from '../type/Task.types';
+import { Footer } from './Footer';
+import { FormTask } from './Form';
 
 import { Task } from './Task';
 
-export function TaskList(props: any) {
+
+export const TaskList = (props: any) => {
   const [tasks, setTasks] = useState([
     {
       id: 'alksdjf',
@@ -17,10 +20,35 @@ export function TaskList(props: any) {
     }
   ])
 
+  const createTask = (task: TaskData) => {
+    setTasks(previousState => ([...previousState, task]))
+  }
+
+  const updateTask = (task: TaskData) => {
+    setTasks(previousState => {
+      return previousState.map(t => (t.id === task.id ? task : t))
+    }
+    )
+  }
+
+  const deleteTask = (task: TaskData) => {
+    setTasks(previousState => {
+      return previousState.filter(t => t.id !== task.id)
+    })
+  }
+
+  const filter = (status: boolean) => {
+    console.log('hi')
+    setTasks(previousState => {
+      return previousState.filter(t => t.status === status)
+    })
+  }
 
   return (
     <div>
-      {tasks.map(({ id, name, status }) => (<Task status={status} name={name} id={id} onChange={}/>))}
+      <FormTask onCreate={createTask} />
+      {tasks.map((t) => (<Task task={t} onChange={updateTask} key={t.id} onClick={deleteTask} />))}
+      <Footer onClick={filter} />
     </div>
   )
 }
